624ee0d4ef2c6a6bcdad9132252adb69ad1bcfa5
Snehal Indurkar <sneha774@live.com>
2022-01-17 11:33:11 -0600

    Initial server and client

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+let state = {
+    preload: function() {
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        game.stage.backgroundColor = '#fee';
+    },
+    create: function() {
+
+    },
+    create: function() {
+
+    },
+};
+
+let game = new Phaser.Game(600, 400, Phaser.CANVAS, null, state);
+console.log("I am more awesome");
